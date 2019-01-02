0ad04de88789d3c5bbe9190bbac8b6bec35131e2
amit1952 <amitavadg@gmail.com>
2019-01-02 10:46:43 +0530

    Use a ternary expression to show the appropriate response.

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,17 +1,23 @@
-import React from 'react';
+import React from "react";
 
 const getSeason = (lat, month) => {
   if (month > 2 && month < 9) {
-    return lat > 0 ? 'summer' : 'winter';
+    return lat > 0 ? "summer" : "winter";
   } else {
-    return lat > 0 ? 'winter' : 'summer';
+    return lat > 0 ? "winter" : "summer";
   }
 };
 
 const SeasonDisplay = props => {
   const season = getSeason(props.lat, new Date().getMonth());
-  console.log(season);
-  return <div>Season Display</div>
+
+  return (
+      <div>
+        <h1>
+          {season === "winter" ? "Brrr! It's chilly!" : "Let's hit the beach!"}
+        </h1>
+      </div>
+  );
 };
 
 export default SeasonDisplay;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
-import ReactDOM from 'react-dom';
-import SeasonDisplay from './SeasonDisplay';
+import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
 
 class App extends React.Component {
   // Constructor
@@ -10,31 +10,28 @@ class App extends React.Component {
       this.state = {lat: null, errorMessage: ''};
     }*/
 
-  state = {lat: null, errorMessage: ''};
+  state = {lat: null, errorMessage: ""};
 
   // Lifecycle method
   componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-        (position) => this.setState({lat: position.coords.latitude}),
-        (err) => this.setState({errorMessage: err.message})
+        position => this.setState({lat: position.coords.latitude}),
+        err => this.setState({errorMessage: err.message})
     );
   }
 
   // React says we have to define render()
   render() {
     if (this.state.errorMessage && !this.state.lat) {
-      return <div>Error: {this.state.errorMessage}</div>
+      return <div>Error: {this.state.errorMessage}</div>;
     }
 
     if (!this.state.errorMessage && this.state.lat) {
-      return <SeasonDisplay lat={this.state.lat}/>
+      return <SeasonDisplay lat={this.state.lat}/>;
     }
 
     return <div>Loading...</div>;
   }
 }
 
-ReactDOM.render(
-    <App/>,
-    document.querySelector('#root')
-);
+ReactDOM.render(<App/>, document.querySelector("#root"));
